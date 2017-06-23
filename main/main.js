6fb25cbaad451ae600c96041fcfbf42e2bd86f6d
zykee <3349567853@qq.com>
2017-06-24 01:41:07 +0800

    test5

--- main/main.js
+++ main/main.js
@@ -2,7 +2,13 @@ const loadAllItems = require('./loadAllItems.js');
 
 module.exports = function main() {
     console.log("Debug Info");
-    return 'Hello World!';
+    return '***<没钱赚商店>购物清单***\n' +
+            '名称：可口可乐，数量：5瓶，单价：3.00(元)，小计：15.00(元)\n' +
+            '名称：雪碧，数量：2瓶，单价：3.00(元)，小计：6.00(元)\n' +
+            '名称：电池，数量：1个，单价：2.00(元)，小计：2.00(元)\n' +
+            '----------------------\n' +
+            '总计：23.00(元)\n' +
+            '**********************';
 };
 
     function printInventory(inputs) {  
